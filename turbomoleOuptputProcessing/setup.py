d550a88c6cc3d6d0e4996b90f0d2bc9c1d20c087
Matthias Blaschke <mblaschke@mun.ca>
2020-08-13 15:53:16 +0200

    init

---
+++ turbomoleOuptputProcessing/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='turbomoleOuptputProcessing',
+    url='https://github.com/jladan/package_demo',
+    author='Matthias Blaschke',
+    author_email='matthias.blaschke@student.uni-augsburg.de',
+    # Needed to actually package something
+    packages=['turbomoleOuptputProcessing'],
+    # Needed for dependencies
+    install_requires=['numpy', 'scipy', 're', 'functools', 'multiprocessing'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    description='Package for processing turbomole Output such as mos files',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
