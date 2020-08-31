93633e3786996b701e88e86edd9720a4efe5ca1f
Matthias Blaschke <mblaschke@mun.ca>
2020-08-31 15:43:47 +0200

    back to state without numba

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     # Needed to actually package something
     packages=['turbomoleOutputProcessing'],
     # Needed for dependencies
-    install_requires=['numpy', 'scipy','numba'],
+    install_requires=['numpy', 'scipy'],
     # *strongly* suggested for sharing
     version='0.1',
     # The license can be anything you like
